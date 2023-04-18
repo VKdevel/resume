8a35ec3259fc78b43415a4d09cd92681f795163f
VKdevel <vitaliykosoy@gmail.com>
2023-04-18 14:01:58 +0300

    fix index

--- src/route/index.js
+++ src/route/index.js
@@ -1871,12 +1871,6 @@ router.get('/shopprofile', function (req, res) {
   })
   //                  ↑↑ сюди вводимо JSON дані
 })
-
-// Підключаємо технологію express для back-end сервера
-const express = require('express')
-// Cтворюємо роутер - місце, куди ми підключаємо ендпоїнти
-const router = express.Router()
-
 // ================================================================
 
 // router.get Створює нам один ентпоїнт
